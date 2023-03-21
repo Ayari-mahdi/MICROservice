61a466f5dcb2684984360e81235b9f683e4d31c6
mahdi ayari <ayarii.mahdii@gmail.com>
2023-03-21 14:19:14 +0100

    first commit

--- MICROSERVICE/MS/src/main/java/com/example/microservice1/Repo/ICandidateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.microservice1.Repo;
-
-import com.example.microservice1.Entity.Candidat;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICandidateRepository  extends CrudRepository<Candidat,Long> {
-}
